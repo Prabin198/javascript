c1369c84ff83ed05ac57b400db0f21e828496be1
Student Prabin Karki <karkiprabin198@gmail.com>
2024-08-02 23:05:46 +0545

    modified

--- basics_03/tempCodeRunnerFile.js
+++ basics_03/tempCodeRunnerFile.js
@@ -1,7 +1,7 @@
 function passObj(anyobject){
-//       console.log(`my name is ${anyobject.name}.I am ${anyobject.age} years old.`)
-// }
-// passObj({
-//     name:"prabin",
-//     age:20
-// })
+       console.log(`my name is ${anyobject.name}.I am ${anyobject.age} years old.`)
+}
+passObj({
+    name:"prabin",
+     age:20
+})
